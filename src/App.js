60ad6444ce413a9cf07106d94c0a98e85a73e235
RedCDawn <redcdawn@MBPro.lan>
2020-11-02 00:22:20 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    // super() used to call the component constructor()
+    super();
+    this.state = {
+      // Array of Objects
+      monsters: [], 
+      searchField: ''
+    };
+
+  }
+  // Life Cycle Function - Loads users 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  }
+  // arrow function auto bind the this context to the place they were defined in the 1st place
+  // lexical scoping
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    // filters monsters depending what you type in searchField, re-render everytime - toLowareCase for case sensitivity
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return ( 
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox 
+          placeholder='search monsters'
+          onSearchChange={this.onSearchChange}
+        />
+          {/* passing filteredMonsters function for listing */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
