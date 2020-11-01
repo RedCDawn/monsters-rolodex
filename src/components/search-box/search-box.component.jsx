1d0cc12b75f5aca4af9362e6bb68f4fb42c33afb
RedCDawn <redcdawn@MBPro.lan>
2020-11-02 00:36:43 +0100

    Adding files for GitHub pages

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -4,12 +4,12 @@ import './search-box.style.css';
 // functional component
 // don't have access to state and therfore constructor - also no access to Life-cycle methods
 // just render some html, this is the purpose 
-export const SearchBox = ({ placeholder, handleChange}) => (
+export const SearchBox = ({ placeholder, onSearchChange}) => (
     <input
         className='search'
         type='search' 
         placeholder={placeholder}
-        onChange={handleChange} 
+        onChange={onSearchChange} 
         // onChange={e => this.setState({ searchField: e.target.value })} 
     />
 );
